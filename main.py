cad0a5ace58963bf13ef347c62838a4f5200ef40
amit-15 <amitkumartiwaree.akt@gmailcom>
2023-03-28 20:43:11 +0530

    training pipeline done

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from thyroid.configuration.mongodb_connection import MongoDBClient
+from thyroid.exception import ThyroidException
+import os, sys
+from thyroid.logger import logging
+from thyroid.pipeline import training_pipeline
+from thyroid.pipeline.training_pipeline import TrainPipeline 
+
 
 if __name__ == '__main__':
-    mongodb_client = MongoDBClient()
-    print("collection name:", mongodb_client.database.list_collection_names())
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
